215c8e1cf9559a0d257cdd7f82be1d510bba4f60
Mohan <mohan@gmail.com>
2018-04-18 20:37:51 +0530

    changed code

---
+++ src/main/java/com/eportal/App.java
@@ -0,0 +1,13 @@
+package com.eportal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Vinod Kumar Ranganatha!" );
+    }
+}
